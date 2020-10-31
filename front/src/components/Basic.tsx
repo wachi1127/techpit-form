2bf9f21cc7038f57f79ebfb655297586b5b310ed
wachi <watakanapinot1127@gmail.com>
2020-10-31 10:48:11 +0900

    Basicのredux管理を設定

--- front/src/components/Basic.tsx
+++ front/src/components/Basic.tsx
@@ -1,21 +1,43 @@
 // Basicは基本情報の入力を管理するコンポーネント
 import React from 'react';
+// reduxに接続
+import { useDispatch, useSelector } from 'react-redux';
 import { TextField } from '@material-ui/core';
 
 import useStyles from './styles';
+import { RootState } from '../domain/entity/rootState';
+
+import { Profile } from '../domain/entity/profile';
+import profileActions from '../store/profile/actions';
 
 const Basic = () => {
+  // dispatchを使用可能にしてくれる関数を生成する = useDispatch
+  const dispatch = useDispatch();
+  const profile = useSelector((state: RootState) => state.profile);
+
   const classes = useStyles();
+  // 更新したい項目だけを受け取ってreducerにdispatchしている
+  const handleChange = (member: Partial<Profile>) => {
+    dispatch(profileActions.setProfile(member));
+  };
 
   return (
     <>
-      <TextField fullWidth className={classes.formField} label="名前" />
+      <TextField
+        fullWidth
+        className={classes.formField}
+        label="名前"
+        value={profile.name}
+        onChange={(e) => handleChange({ name: e.target.value })}
+      />
       <TextField
         fullWidth
         multiline
         className={classes.formField}
         rows={5}
         label="自己紹介"
+        value={profile.description}
+        onChange={(e) => handleChange({ description: e.target.value })}
       />
     </>
   );
